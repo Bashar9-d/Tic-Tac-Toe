78be0eceeb6b148666de2df52dcfe80a5da6a8e5
Bashar9-d <busharshaqoor922@gmail.com>
2025-05-18 02:09:28 +0300

    c

--- lib/splash.dart
+++ lib/splash.dart
@@ -1,7 +1,7 @@
 
 import 'package:avatar_glow/avatar_glow.dart';
 import 'package:flutter/material.dart';
-import 'package:url_launcher/url_launcher.dart';
+
 
 import 'HomePage.dart';
 
@@ -15,16 +15,10 @@ class Splash extends StatelessWidget {
       body:Center(child: Column(mainAxisAlignment: MainAxisAlignment.spaceAround,children: [
         Text("Tic Tac Toe",style: TextStyle(color: Colors.white,fontSize: 50),),
         AvatarGlow(child: Image.asset("assets/tictactoelogo.png",height: 200,width: 200,), ),
-        InkWell(
-            onTap: () {
-            launchUrl(  Uri.parse(
-                "https://www.linkedin.com/in/bashar-shaqour-320b922a7/",
-              ));
-            },
-            child: Text(
+        Text(
               "@Created By Bashar",
               style: TextStyle(color: Colors.white, fontSize: 30),
-            ),),
+            ),
         FilledButton(onPressed: (){Navigator.of(context).push(MaterialPageRoute(builder: (context)=>HomePage()));},style: ButtonStyle(backgroundColor: MaterialStateProperty.all<Color>(Colors.white),), child: Text("Start Game",style: TextStyle(color: Colors.black,fontSize: 30),),)
       ]),)
     );
